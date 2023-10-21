33fd8a90c07339112684346a89db111e56fe51e6
Newonprogrraming <armen2002aaa@gmail.com>
2023-10-22 01:54:52 +0300

    Update 4

--- tinkoffcup/src/components/Table/index.jsx
+++ tinkoffcup/src/components/Table/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './style.module.css'
+import {ModelWindow} from '../ModelWindow';
+import Card from '../Card';
 
 function Component(props) {
+    const [isModal, setModal] = React.useState(false);
     return (
         <div className={styles.column}>
-            <div className={styles.row1}>
+            <ModelWindow
+            isVisible={isModal}
+            title="Modal Title"
+            content={<p>Hello</p>}
+            onClose={() => setModal(false)}
+            />
+            <div className={styles.row1} onClick={() => setModal(true)}>
                 <p>Ник: {props.post.nick_name}</p>
                 <p>ID: {props.post.id}</p>
             </div>
-            <div className={styles.row2}>
+            <div className={styles.row2} onClick={() => setModal(true)}>
                 <p>Ник: {props.post.nick_name}</p>
                 <p>ID: {props.post.id}</p>
             </div>
